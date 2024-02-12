9b8dd4cff6bec69e8fb1f4015b9aa1c6757b4387
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-12 22:35:42 +0530

    learned about operators in php

--- index.php
+++ index.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        h1{
+            color:red;
+            background-color:lightblue;
+        }
+    </style>
 </head>
 <body>
     hello this is php website
     <?php
-    echo "hello this is written in php";
+    echo "hello this is written in php <br>";
+    echo "<h1>this is a heading</h1>";
+    $variable1 = 7;
+    $variable2=5;
+    echo "variable1=$variable1<br>variable2=$variable2<br>";
+    echo "$variable1+$variable2=",$variable1+$variable2,"<br>";
+    echo "$variable1-$variable2=",$variable1-$variable2,"<br>";
+    echo "$variable1*$variable2=",$variable1*$variable2,"<br>";
+    echo "$variable1/$variable2=",$variable1/$variable2,"<br>";
+    
     ?>
 </body>
 </html>
